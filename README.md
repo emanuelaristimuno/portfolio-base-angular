b542c7582b39069c073be14b87cc04ea822bec1f
emanuelaristimuno <emanueluader@gmail.com>
2022-01-24 16:31:45 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# github-pww3m9-hvy8ts
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/github-pww3m9-hvy8ts)
---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
+import { BannerComponent } from './componentes/banner/banner.component';
+import { AcercaDeComponent } from './componentes/acerca-de/acerca-de.component';
+import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
+import { SkillsComponent } from './componentes/skills/skills.component';
+import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EducacionComponent,
+    NavbarComponent,
+    BannerComponent,
+    AcercaDeComponent,
+    ExperienciaComponent,
+    SkillsComponent,
+    ProyectosComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
